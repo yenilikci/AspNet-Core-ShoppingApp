37da819ab14635e3bcbf85dcc50423b673dd6980
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-21 23:55:41 +0300

    Proje Oluşturuldu

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ShoppingApp.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
