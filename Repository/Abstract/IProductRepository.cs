7238d0697ba66526b71759df495849b785aa2856
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-22 00:12:16 +0300

    Entityler Oluşturuldu

---
+++ Repository/Abstract/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.WebUI.Repository.Abstract
+{
+    public class IProductRepository
+    {
+    }
+}
