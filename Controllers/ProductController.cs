83daf41a582596c16f93033d7a8bb78aa7709ff3
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-22 23:45:41 +0300

    Product Details Tamamlandı

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingApp.WebUI.Models;
+using ShoppingApp.WebUI.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(repository
+                .GetAll()
+                .Where(i => i.ProductId == id)
+                .Include(i => i.Images)
+                .Include(i => i.Attributes)
+                .Include(i => i.ProductCategories)
+                .ThenInclude(i => i.Category)
+                .Select(i => new ProductDetailsModel()
+                {
+                    Product = i,
+                    Images = i.Images,
+                    ProductAttributes = i.Attributes,
+                    Categories = i.ProductCategories.Select(a => a.Category).ToList()
+                })
+                .FirstOrDefault());
+        }
+    }
+}
---
+++ Models/ProductDetailsModel.cs
@@ -0,0 +1,16 @@
+﻿using ShoppingApp.WebUI.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.WebUI.Models
+{
+    public class ProductDetailsModel
+    {
+        public Product Product { get; set; }
+        public List<Image>Images { get; set; }
+        public List<ProductAttribute> ProductAttributes { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
