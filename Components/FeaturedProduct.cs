ea1644a7277dff85defa312afce9a48df3df85da
Melih Çelik <57464067+yenilikci@users.noreply.github.com>
2020-12-22 19:46:36 +0300

    FeaturedProduct View Component Oluşturuldu

---
+++ Components/FeaturedProduct.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.WebUI.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApp.WebUI.Components
+{
+    public class FeaturedProduct : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public FeaturedProduct(IProductRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.GetAll().Where(i => i.isApproved && i.isFuture).ToList());
+        }
+
+
+    }
+}
